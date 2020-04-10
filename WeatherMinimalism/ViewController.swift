34e8c9c941dd49e881da1c0bfa11a41b8e8d418d
Max <maxzheleznyy@gmail.com>
2020-04-10 12:11:14 -0700

    Initial Commit

---
+++ WeatherMinimalism/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherMinimalism
+//
+//  Created by Maxim Zheleznyy on 4/10/20.
+//  Copyright © 2020 Maxim Zheleznyy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
